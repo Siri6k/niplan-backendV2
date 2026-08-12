8719dfa7343768c4299a808c873cc301614db500
sirisk <adamchrisk@gmail.com>
2026-08-12 14:13:17 +0200

    Remove listing app and all associated files, including models, views, serializers, migrations, and tests. Introduce an empty __init__.py file in the marketplace controllers directory.

--- listing/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ListingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'listing'
-
-    def ready(self):
-        import listing.signals  
